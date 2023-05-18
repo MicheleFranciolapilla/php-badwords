705df77b84e715681910cfaa43ca1e03fcc472f4
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-05-18 22:07:26 +0900

    index.php in costruzione

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP BadWords</title>
+</head>
+<body>
+    <header style="text-align:center">
+        <h1>Benvenuto nel mio primo programma in PHP</h1>
+    </header>
+    <main>
+        <form action="" style="padding-left:30px; font-size:large; font-weight:800;">
+            <label for="paragraph" style="color: black;">Paragrafo</label>
+        </form>
+    </main>
+</body>
+</html>
